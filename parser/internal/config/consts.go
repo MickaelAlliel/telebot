b11706db2c8568b9649711a4d4af20cd32b07ef0
Mickael Alliel <mickael.alliel@gmail.com>
2022-05-20 16:50:39 +0300

    improve reply

--- parser/internal/config/consts.go
+++ parser/internal/config/consts.go
@@ -8,5 +8,7 @@ const (
 	ErrorMessage_BadRequest       = "Oh no! It seems your message wasn't in the right format.\nRequired:\namount (10 | 10.5)\ncategory\npayment method"
 	ErrorMessage_FailedToSave     = "Oh no! I couldn't save your data...\nPlease try again or contact my owner."
 
-	SuccessMessage_Saved = ":cash: :+1:"
+	// https://apps.timwhitlock.info/emoji/tables/unicode
+	// Emojis can be sent using UNICODE values
+	SuccessMessage_Saved = "\xF0\x9F\x92\xB0 \xF0\x9F\x91\x8D" // :moneybag: :thumbsup:
 )
