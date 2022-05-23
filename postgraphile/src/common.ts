2a35c7e65c0ca3326dab86b623090d2683b7af58
Mickael Alliel <mickael.alliel@gmail.com>
2022-05-23 10:02:02 +0300

    postgraphile as code jwt not working yet

---
+++ postgraphile/src/common.ts
@@ -0,0 +1,108 @@
+import { Pool } from 'pg';
+import {
+  PostGraphileOptions,
+  makePluginHook,
+  makeExtendSchemaPlugin,
+  gql,
+} from 'postgraphile';
+import PgPubsub from '@graphile/pg-pubsub';
+import PgSimplifyInflectorPlugin from '@graphile-contrib/pg-simplify-inflector';
+import PostGraphileConnectionFilterPlugin = require('postgraphile-plugin-connection-filter');
+
+// @ts-ignore `@graphile/pg-pubsub` pulls types from npm `postgraphile` module rather than local version.
+const pluginHook = makePluginHook([PgPubsub]);
+
+const MySubscriptionPlugin = makeExtendSchemaPlugin((build) => {
+  return {
+    typeDefs: gql`
+      type TimePayload {
+        currentTimestamp: String
+        query: Query
+      }
+      extend type Subscription {
+        time: TimePayload @pgSubscription(topic: "time")
+      }
+    `,
+    resolvers: {
+      Subscription: {
+        time(event) {
+          return event;
+        },
+      },
+      TimePayload: {
+        query() {
+          return build.$$isQuery;
+        },
+      },
+    },
+  };
+});
+
+// Connection string (or pg.Pool) for PostGraphile to use
+export const getDatabaseConnectionConfig: () => string | Pool = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      process.env.DATABASE_URL ||
+      'postgres://postgres:postgrespassword@localhost:5432/postgres'
+    );
+  }
+
+  if (process.env.DB_UNIX_SOCKET) {
+    return new Pool({
+      host: `${process.env.DB_UNIX_SOCKET}/${process.env.DB_HOST}`,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+  }
+
+  return new Pool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+};
+
+// Database schemas to use
+export const schemas: string | string[] = ['public'];
+
+// PostGraphile options; see https://www.graphile.org/postgraphile/usage-library/#api-postgraphilepgconfig-schemaname-options
+export const options: PostGraphileOptions = {
+  pluginHook,
+  appendPlugins: [
+    MySubscriptionPlugin,
+    PgSimplifyInflectorPlugin,
+    PostGraphileConnectionFilterPlugin,
+  ],
+  pgSettings(req) {
+    // Adding this to ensure that all servers pass through the request in a
+    // good enough way that we can extract headers.
+    // CREATE FUNCTION current_user_id() RETURNS text AS $$ SELECT current_setting('graphile.test.x-user-id', TRUE); $$ LANGUAGE sql STABLE;
+    return {
+      'graphile.test.x-user-id':
+        req.headers['x-user-id'] ||
+        // `normalizedConnectionParams` comes from websocket connections, where
+        // the headers often cannot be customized by the client.
+        (req as any).normalizedConnectionParams?.['x-user-id'],
+    };
+  },
+  watchPg: true,
+  graphiql: true,
+  enhanceGraphiql: true,
+  subscriptions: true,
+  dynamicJson: true,
+  setofFunctionsContainNulls: false,
+  ignoreRBAC: false,
+  showErrorStack: 'json',
+  extendedErrors: ['hint', 'detail', 'errcode'],
+  allowExplain: true,
+  legacyRelations: 'omit',
+  exportGqlSchemaPath: `${__dirname}/schema.graphql`,
+  sortExport: true,
+};
+
+export const port: number = process.env.PORT
+  ? parseInt(process.env.PORT, 10)
+  : 5000;
---
+++ postgraphile/src/server.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S npx ts-node
+import express = require('express');
+import { postgraphile } from 'postgraphile';
+import { getDatabaseConnectionConfig, schemas, options, port } from './common';
+import { expressjwt } from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+
+const middleware = postgraphile(
+  getDatabaseConnectionConfig(),
+  schemas,
+  options
+);
+
+const checkJwt = expressjwt({
+  secret: expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://YOUR_DOMAIN/.well-known/jwks.json`,
+  }),
+  audience: 'YOUR_API_IDENTIFIER',
+  issuer: `https://YOUR_DOMAIN/`,
+  algorithms: ['RS256'],
+});
+
+const app = express();
+app.use(middleware);
+
+const server = app.listen(port, () => {
+  const address = server.address();
+  if (typeof address !== 'string') {
+    const href = `http://127.0.0.1:${address.port}${
+      options.graphiqlRoute || '/graphiql'
+    }`;
+    console.log(`PostGraphiQL available at ${href} 🚀`);
+  } else {
+    console.log(`PostGraphile listening on ${address} 🚀`);
+  }
+});
